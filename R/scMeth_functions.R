b3eea8f6879ef1dc6e0890640efaa9b16c507543
hui-tony-zk <hui.tony.zk@gmail.com>
2018-01-15 16:53:19 -0800

    Add functions

--- R/scMeth_functions.R
+++ R/scMeth_functions.R
@@ -8,7 +8,6 @@ cores_to_use <- 30
 #' @param ncores CPUs to parallelize over (must be >1)
 #'
 #' @return Returns a data.frame with two columns (from and to) and rows equal to the number of cores
-#' @export
 #'
 #' @examples divide_cores(total = 100, ncores = 2)
 #'
@@ -46,11 +45,11 @@ divide_cores <- function(total, ncores = cores_to_use) {
 #' @param cpg A list of named data frames containing CpG calls. See details for required format of dataframes. Required.
 #' @param digital Whether or not to discard non-binary CpG calls. Useful in single-cells as it's very unlikely that a single-cell contains a heterozygous methylation call. Defaults to TRUE.
 #' @param ncores Number of cores to parallelize over. Defaults to 1
-#' @param calcdiff Whether or not to directly calculate the average difference (if TRUE), or to return a list of dataframes containing pairwise common CpGs (if FALSE). Defaults to TRUE.
+#' @param calcdiff Whether or not to directly calculate the average difference (if TRUE), or to return a list of dataframes containing pairwise common CpGs (if FALSE). Defaults to TRUE. Usually you don't want false unless you wish to do something else with all the pairwise data
 #'
 #' @return A list of dataframes if \code{calcdiff} is FALSE. Otherwise, a dataframe containing the pairwise dissimiarlties if \code{calcdiff} is TRUE.
 #' @export
-#' @import foreach dplyr
+#' @import stats foreach doMC dplyr
 #'
 #' @details
 #' Each dataframe containing CpG calls must have the following four columns:
@@ -67,8 +66,7 @@ divide_cores <- function(total, ncores = cores_to_use) {
 create_pairwise_master <- function(cpg, digital = TRUE, ncores = 2, calcdiff = TRUE){
   doMC::registerDoMC(ncores)
   # Generate combinations
-  comb <- combn(length(names(cpg)),2)
-  comb.names <- combn(names(cpg),2)
+  comb.names <- utils::combn(names(cpg),2)
   total_batch <- divide_cores(ncol(comb.names), ncores)
   # Combine
   pairwise <- foreach(i=1:nrow(total_batch), .combine = c) %dopar% {
@@ -99,12 +97,12 @@ create_pairwise_master <- function(cpg, digital = TRUE, ncores = 2, calcdiff = T
   return(pairwise)
 }
 
-#' Get manhattan distance from pairwise common data frame
+#' Get Dissimilarity from a pairwise common data frame
 #'
 #' @param df A dataframe containing the pairwise common CpGs. Required.
 #'
 #' @return A 1-row dataframe containing the pairwise dissimilarity
-#' @export
+#' @import stats dplyr
 #'
 #' @details
 #' Typically this function is not used by the user. It is called by \code{create_pairwise_master} when \code{calc_diff} is TRUE.
@@ -115,30 +113,31 @@ create_pairwise_master <- function(cpg, digital = TRUE, ncores = 2, calcdiff = T
 #' 3. Name of the 1st comparitor (e.g. cell1)
 #' 4. Name of the 2nd comparitor (e.g. cell2)
 #'
+
 get_diff_df <- function(df) {
   diff.temp <- data.frame(
     x = names(df)[3],
     y = names(df)[4],
     total = nrow(df),
     pear_corr = c(cor(df[[3]], df[[4]], method = "pearson")),
-    manhattan_dist = sum(abs(df[[3]]-df[[4]]))
+    pairwise_dissimilarity_total = sum(abs(df[[3]]-df[[4]]))
   ) %>%
-    mutate(
-      manhattan_dist_scaled = manhattan_dist/total
-    )
+    mutate(pairwise_dissimilarity = pairwise_dissimilarity_total/total)
   return(diff.temp)
 }
 
 #' Convert pairwise dissimilarity data.frame to a pairwise matrix
 #'
-#' @param master_diff The data.frame containing pairwise dissimilarites. Typically the output of \code{create_pairwise_master}. See details for format of this data.frame. Required.
-#' @param measure The name of the column to use as the values for the pairwise dissimilarity matrix. Defaults to the manhanttan distance. Can be changed if user wants another measure (e.g. correlation, etc).
+#' @param master_diff The data.frame containing pairwise dissimilarites. Typically the output of \code{create_pairwise_master}. Required.
+#' @param measure The name of the column to use as the values for the pairwise dissimilarity matrix. Defaults to the pairwise_dissimilarity. Can be changed if user wants another measure (e.g. correlation, etc).
 #' @param diag The value to put in the diagonal. Defaults to NA.
 #' @param sample_subset A character vector of sample names to subset. Optional.
 #'
 #' @return A data.matrix representing the pairwise dissimilarites between every cell
 #' @export
-convert_to_distance_matrix <- function(master_diff, measure = "manhattan_dist_scaled", diag = NA, sample_subset = NULL) {
+#' @import dplyr
+#'
+convert_to_distance_matrix <- function(master_diff, measure = "pairwise_dissimilarity", diag = NA, sample_subset = NULL) {
   if (! is.null(sample_subset)) {
     master_diff <- master_diff %>% filter(x %in% sample_subset, y %in% sample_subset)
   }
@@ -149,13 +148,27 @@ convert_to_distance_matrix <- function(master_diff, measure = "manhattan_dist_sc
   test.same <- data.frame(x=list_samples,y=list_samples,value=diag)
   test <- unique(rbind(test.same, test, test.rev))
 
-  x <- data.matrix(spread(test, key = y, value = value)[,-1])
+  x <- data.matrix(tidyr::spread(test, key = y, value = value)[,-1])
   rownames(x) <- colnames(x)
-
   return(x)
 }
 
+#' Create in silico merged bulk profiles from single-cell files
+#'
+#' @param cluster_members A vector of samples to include
+#' @param cpg_all The list containing all CpG calls in data.frame format
+#'
+#' @return a BSmooth object ready for smoothing
+#' @import stats
+#' @export
+#'
+#' @details Uses the bsseq package to perform in silico merging of single-cell CpG calls. Requires the bsseq R package to be installed
+#'
 merge_cpgs <- function(cluster_members, cpg_all) {
+  if (!requireNamespace("bsseq", quietly = TRUE)) {
+    stop("'bsseq' package needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
   # usage: bssmooth_list <- mclapply(cluster_groupings, merge_cpgs)
   tmp <- cpg_all[get(cluster_members)]
   tmp <- lapply(seq_along(tmp), function(i) {
